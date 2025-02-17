b24959d1ef71ea804df980fad7a03320e4044cac
Nikita Skulkin <clonerpresent@gmail.com>
2025-02-17 17:14:09 +0300

    add guides menu

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -56,6 +56,7 @@ export default {
     nav: [
       { text: 'Главная', link: '/' },
       { text: 'Проекты', link: '/projects' },
+      { text: 'Гайды', link: '/guides' },
       { text: 'GitHub', link: 'https://github.com/sklknn' },
     ],
 
